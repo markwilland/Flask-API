f424eb5399c6c8447409cef97b1c6c485bde64fb
markwilland <markwillanderson@gmail.com>
2023-03-08 01:06:49 -0500

    get method

--- projects/api/api.py
+++ projects/api/api.py
@@ -5,23 +5,12 @@ import sqlite3
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+def dict_factory(cursor, row):
+        d = {}
+        
+        for idx, col in enumerate(cursor.description):
+                d[col[0]] = row[idx]
+        return d
 
 @app.route('/', methods=['GET'])
 def home():
@@ -29,7 +18,12 @@ def home():
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+    
+    return jsonify(all_books)
 
 @app.route('/api/v1/resources/books', methods=['GET'])
 def api_id():
