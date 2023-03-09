013076623df64e3e32726ae401d4ef00f758dc7a
markwilland <markwillanderson@gmail.com>
2023-03-09 00:23:47 -0500

    fixed spacing to meet pep8

--- projects/api/api.py
+++ projects/api/api.py
@@ -5,17 +5,20 @@ import sqlite3
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 def dict_factory(cursor, row):
-        d = {}
-        
-        for idx, col in enumerate(cursor.description):
-                d[col[0]] = row[idx]
-                
-        return d
+    d = {}
+
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+
+    return d
+
 
 @app.route('/', methods=['GET'])
 def home():
-        return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 
 @app.route('/api/v1/resources/referrals/all', methods=['GET'])
 def get_all_referrals():
@@ -23,85 +26,87 @@ def get_all_referrals():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     referrals = cur.execute('SELECT * FROM referrals;').fetchall()
-    
+
     return jsonify(referrals)
+
 
 @app.route('/api/v1/resources/referrals/<int:referral_id>', methods=['GET'])
 def get_referral_by_id(referral_id):
-        print("Referal Id Provided: " + str(referral_id))
-        
-        if referral_id is None or referral_id == 0:
-                return jsonify({'message': 'Error: No id field provided. Please specify an id.'}), 400
-        
-        
-        conn = sqlite3.connect('referrals.db')
-        cur = conn.cursor()
-        
-        select_sql = '''SELECT * FROM referrals WHERE id = ?'''
-        
-        cur.execute(select_sql, (referral_id,))
-        
-        referral = cur.fetchone()
-        
-        conn.close()
-        
-        if referral is None:
-                return jsonify({'message': f'Referral with Id {referral_id} not found'}), 404
-        
-        referral_dict = {
-                'id': referral[0],
-                'firm_code': referral[1],
-                'file_type': referral[2],
-                'name': referral[3],
-                'email': referral[4],
-                'phone': referral[5],
-                'street_address_1': referral[6],
-                'street_address_2': referral[7],
-                'city': referral[8],
-                'province': referral[9],
-                'postal_code': referral[10]
-        }
+    print("Referal Id Provided: " + str(referral_id))
 
-        return jsonify({'message': 'Referral found', 'referral': referral_dict})
-                        
+    if referral_id is None or referral_id == 0:
+        return jsonify({'message': 'Error: No id field provided. Please specify an id.'}), 400
+
+    conn = sqlite3.connect('referrals.db')
+    cur = conn.cursor()
+
+    select_sql = '''SELECT * FROM referrals WHERE id = ?'''
+
+    cur.execute(select_sql, (referral_id,))
+
+    referral = cur.fetchone()
+
+    conn.close()
+
+    if referral is None:
+        return jsonify({'message': f'Referral with Id {referral_id} not found'}), 404
+
+    referral_dict = {
+        'id': referral[0],
+        'firm_code': referral[1],
+        'file_type': referral[2],
+        'name': referral[3],
+        'email': referral[4],
+        'phone': referral[5],
+        'street_address_1': referral[6],
+        'street_address_2': referral[7],
+        'city': referral[8],
+        'province': referral[9],
+        'postal_code': referral[10]
+    }
+
+    return jsonify({'message': 'Referral found', 'referral': referral_dict})
+
+
 @app.route('/api/v1/resources/referrals', methods=['POST'])
 def api_new():
-        print("Post Request Incoming")
-        data = request.get_json()
-        print(data)
-        
-        conn = sqlite3.connect('referrals.db')
-        cur = conn.cursor()
-        
-        insert_statement = """
-                INSERT INTO referrals (firm_code, file_type, name, email, phone, street_address_1, street_address_2, city, province, postal_code)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""" 
-        
-        insert_data = (
-                data['firm_code'],
-                data['file_type'],
-                data['name'],
-                data['email'],
-                data['phone'],
-                data['street_address_1'],
-                data['street_address_2'],
-                data['city'],
-                data['province'],
-                data['postal_code']
-                )
-        print(insert_data)
-        
-        cur.execute(insert_statement, insert_data)
-        
-        referral_id = cur.lastrowid
-        
-        print(referral_id)
-        
-        conn.commit()
-        conn.close()
+    print("Post Request Incoming")
+    data = request.get_json()
+    print(data)
 
-        print("Record inserted")
-        
-        return jsonify({'message': 'Referral added successfully!', 'referral_id': referral_id})
+    conn = sqlite3.connect('referrals.db')
+    cur = conn.cursor()
+
+    insert_statement = """
+                INSERT INTO referrals (firm_code, file_type, name, email, phone, street_address_1, street_address_2, city, province, postal_code)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+
+    insert_data = (
+        data['firm_code'],
+        data['file_type'],
+        data['name'],
+        data['email'],
+        data['phone'],
+        data['street_address_1'],
+        data['street_address_2'],
+        data['city'],
+        data['province'],
+        data['postal_code']
+    )
+    print(insert_data)
+
+    cur.execute(insert_statement, insert_data)
+
+    referral_id = cur.lastrowid
+
+    print(referral_id)
+
+    conn.commit()
+    conn.close()
+
+    print("Record inserted")
+
+    return jsonify({'message': 'Referral added successfully!', 'referral_id': referral_id})
+
 
 app.run()
